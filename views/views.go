c5c984dc51ffceb04a8ca263277c7fd06663347d
Anders Pearson <anders@columbia.edu>
2012-05-14 10:29:13 -0400

    putting in simple views

--- views/views.go
+++ views/views.go
@@ -1,7 +1,11 @@
 package views
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"html/template"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,14 +13,28 @@ type Page struct {
     Title string
 }
 
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles("templates/" + tmpl + ".html")
+    t.Execute(w, p)
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("templates/index.html")
 	p := Page{Title: "index"}
-	t.Execute(w,p)
+	renderTemplate(w,"index",&p)
 }
 
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("templates/add.html")
-	p := Page{Title: "upload image"}
-	t.Execute(w,p)
+	if r.Method == "POST" {
+		p := Page{Title: "uploaded image"}
+		i, ih, _ := r.FormFile("image")
+		h := sha1.New()
+		d,_ := ioutil.ReadAll(i)
+		io.WriteString(h, d.String())
+		fmt.Printf("% x", h.Sum(nil))
+		fmt.Println(ih.Filename)
+		renderTemplate(w,"add",&p)
+	} else {
+		p := Page{Title: "upload image"}
+		renderTemplate(w,"add",&p)
+	}
 }
