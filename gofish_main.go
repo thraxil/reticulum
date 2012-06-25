70a863be139422af081046cbbbe8490780b6f964
Anders Pearson <anders@columbia.edu>
2012-06-24 20:21:23 -0400

    read JSON config file specified on command line

--- gofish_main.go
+++ gofish_main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"./views"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+type ConfigData struct {
+	Port int64
+	UUID string
+	Name string
+}
+
 func main() {
+	var config string
+	flag.StringVar(&config, "config", "./config.json", "JSON config file")
+  flag.Parse()
+
+	file, err := ioutil.ReadFile(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f := ConfigData{}
+	json.Unmarshal(file, &f)
+
 	http.HandleFunc("/", views.AddHandler)
 	http.HandleFunc("/image/", views.ServeImageHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", f.Port), nil)
 }
