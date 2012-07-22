c51c1b3739fcc4ce86becde8cf0046a2e031e17d
Anders Pearson <anders@columbia.edu>
2012-07-22 12:44:05 -0400

    verifier detects and logs corrupt image files

--- verifier/verifier.go
+++ verifier/verifier.go
@@ -3,18 +3,80 @@ package verifier
 import (
 	"../cluster"
 	"../models"
-	"path/filepath"
-  "os"
+	"crypto/sha1"
+	"errors"
   "fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"log/syslog"
 	"math/rand"
+  "os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
+func basename(path string) string {
+	filename := filepath.Base(path)
+	ext := filepath.Ext(filename)
+	return filename[:len(filename) - len(ext)]
+}
+
+func hashFromPath(path string) (string, error) {
+	dir := filepath.Dir(path)
+	parts := strings.Split(dir,"/")
+	// only want the last 20 parts
+	if len(parts) < 20 {
+		return "", errors.New("not enough parts")
+	}
+	hash := strings.Join(parts[len(parts) - 20:],"")
+	if len(hash) != 40 {
+		return "", errors.New("invalid hash")
+	}
+	return hash, nil
+}
+
 func visit(path string, f os.FileInfo, err error) error {
+	// all we care about is the "full" version of each
 	if f.IsDir() { return nil }
+	if basename(path) != "full" { return nil }
+
 	fmt.Printf("Visited: %s\n", path)
+	hash, err := hashFromPath(path)
+	if err != nil {
+		return nil
+	}
+	fmt.Printf("Hash: %s\n", hash)
+
+	h := sha1.New()
+	d, _ := ioutil.ReadFile(path)
+	io.WriteString(h, string(d))
+	ahash := fmt.Sprintf("%x", h.Sum(nil))
+
+	if hash != ahash {
+		//    if different:
+		//       corruption! 
+		//       log it
+		fmt.Printf("image %s appears to be corrupted!\n", path)
+		//       trust that the hash was correct on upload
+		//       ask other nodes for a copy
+		//       double check that what we get is indeed right
+		//       replace the full-size with a corrected one
+		//       delete all the cached sizes too since they
+		//       may have been created off the broken one
+	}
+
+	//    VERIFY PHASE
+	//    calculate hash
+	//    compare to the path
+  //    REBALANCE PHASE
+	//    calculate the ring for the image
+	//    check that each of the N nodes at the front of the ring have it
+	//    for each:
+	//       if it doesn't have it, stash it
+	//    if we've verified that the first N nodes have it, and the current
+	//      node is not in that first N, delete the local copy
 
 	// TODO: sleep a bit in here
 	return nil
@@ -35,32 +97,11 @@ func Verify (c *cluster.Cluster, s models.SiteConfig) {
 		jitter = rand.Intn(5)
 		time.Sleep(time.Duration(base_time + jitter) * time.Second)
 		sl.Info("verifier starting at the top")
-		// get a list of all the images on our node
 
 		root := s.UploadDirectory
 		err := filepath.Walk(root, visit)
-		fmt.Printf("filepath.Walk() returned %v\n", err)
-
-		// for each:
-		//    VERIFY PHASE
-		//    calculate hash
-		//    compare to the path
-		//    if different:
-		//       corruption! 
-		//       log it
-		//       trust that the hash was correct on upload
-		//       ask other nodes for a copy
-		//       double check that what we get is indeed right
-		//       replace the full-size with a corrected one
-		//       delete all the cached sizes too since they
-		//       may have been created off the broken one
-    //    REBALANCE PHASE
-		//    calculate the ring for the image
-		//    check that each of the N nodes at the front of the ring have it
-		//    for each:
-		//       if it doesn't have it, stash it
-		//    if we've verified that the first N nodes have it, and the current
-		//      node is not in that first N, delete the local copy
-
+		if err != nil {
+			fmt.Printf("filepath.Walk() returned %v\n", err)
+		}
 	}
 }
