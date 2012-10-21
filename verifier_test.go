6c838c148312e68c6231125c678e8ed0c50fb7be
Anders Pearson <anders@columbia.edu>
2012-10-21 18:05:00 -0400

    hashFromPath coverage

---
+++ verifier_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_hashFromPath(t *testing.T) {
+	var path = "30/de/73/dc/ec/0a/b2/de/54/03/5e/dd/a6/43/ad/a6/9d/cd/60/c4/file.jpg"
+	var h = "30de73dcec0ab2de54035edda643ada69dcd60c4"
+	o,err := hashFromPath(path)
+	if err != nil || o != h {
+		t.Error("broked!")
+	}
+	// test the error conditions
+	// 1: not enough parts
+	path = "b2/de/54/03/5e/dd/a6/43/ad/a6/9d/cd/60/c4/file.jpg"
+	o2,err2 := hashFromPath(path)
+	if err2 == nil || o2 != "" {
+		t.Error("error not handled")
+	}
+	// 2: too many parts, no filename
+	path = "30/de/73/dc/ec/0a/b2/de/54/03/5e/dd/a6/43/ad/a6/9d/cd/60/c4/aa/aa/aa/foo"
+	o3,err3 := hashFromPath(path)
+	if err3 == nil || o3 != "" {
+		t.Error("error not handled")
+	}
+	
+}
