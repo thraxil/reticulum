197713c96c35182601b7ee2d911ae6e97a03d2da
Anders Pearson <anders@columbia.edu>
2012-11-02 14:40:16 -0400

    fixed a test case

--- verifier_test.go
+++ verifier_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -18,10 +19,12 @@ func Test_hashFromPath(t *testing.T) {
 	if err2 == nil || o2 != "" {
 		t.Error("error not handled")
 	}
-	// 2: too many parts, no filename
-	path = "30/de/73/dc/ec/0a/b2/de/54/03/5e/dd/a6/43/ad/a6/9d/cd/60/c4/aa/aa/aa/foo"
+	// 2: invalid parts
+	path = "30/de/73/dc/ec/0a/b2/de/54/03/5e/dd/a6/43/ad/a6/9d/cd/60blah/c4/aa/aa/aa/foo.jpg"
 	o3,err3 := hashFromPath(path)
 	if err3 == nil || o3 != "" {
+		fmt.Println(err3)
+		fmt.Println(o3)
 		t.Error("error not handled")
 	}
 	
