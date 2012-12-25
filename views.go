1d598dfbe479b3f4847e3561477b36e294443e47
Anders Pearson <anders@columbia.edu>
2012-12-24 21:55:30 -0500

    extract serveScaledByExtension()

--- views.go
+++ views.go
@@ -190,6 +190,11 @@ func ServeImageHandler(w http.ResponseWriter, r *http.Request, ctx Context) {
 		// we just can't cache it. 
 	}
 	w = setCacheHeaders(w, ri.Extension)
+	ctx.serveScaledByExtension(ri, w, outputImage, wFile)
+}
+
+func (ctx Context) serveScaledByExtension(ri *ImageSpecifier, w http.ResponseWriter,
+	outputImage image.Image, wFile *os.File) {
 	if ri.Extension == ".jpg" {
 		serveJpg(wFile, outputImage, w, ri.sizedPath(ctx.Cfg.UploadDirectory), ctx, ri.MemcacheKey())
 		return
