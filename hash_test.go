a99c07a6e007be58d65f25f43c5414d2ab87524a
Anders Pearson <anders@columbia.edu>
2013-12-08 22:41:09 -0500

    hash.go tests

--- hash_test.go
+++ hash_test.go
@@ -19,4 +19,37 @@ func Test_HashFromString(t *testing.T) {
 	if h.AsPath() != "ae/28/60/5f/0f/fc/34/fe/53/14/34/2f/78/ef/aa/13/ee/45/f6/99" {
 		t.Error(fmt.Sprintf("wrong path: %s", h.AsPath()))
 	}
+	h, err = HashFromString("ae28605f0ffc34e5314342f78efaa13ee45f699", "")
+	if err == nil {
+		t.Error("non 40 char hash should've been an error")
+	}
+}
+
+func Test_Valid(t *testing.T) {
+	h, _ := HashFromString("ae28605f0ffc34fe5314342f78efaa13ee45f699", "")
+	if !h.Valid() {
+		t.Error("hash should be valid")
+	}
+	h.Algorithm = "foo"
+	if h.Valid() {
+		t.Error("hash should not be valid (only sha1 supported for now)")
+	}
+}
+
+func Test_HashFromPath(t *testing.T) {
+	_, err := HashFromPath("ae/28/60/5f/0f/fc/34/fe/53/14/34/2f/78/ef/aa/13/ee/45/f6/99/foo.jpg")
+	if err != nil {
+		t.Error("bad hash")
+	}
+
+	_, err = HashFromPath("ae/28/60/fc/34/fe/53/14/34/2f/78/ef/aa/13/ee/45/f6/99/foo.jpg")
+	if err == nil {
+		t.Error("shouldn't be enough pieces")
+	}
+
+	_, err = HashFromPath("ae/288/60/5f/0f/fc/34/fe/53/14/34/2f/78/ef/aa/13/ee/45/f6/99/foo.jpg")
+	if err == nil {
+		t.Error("not 40 chars")
+	}
+
 }
