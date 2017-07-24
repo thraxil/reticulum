0a36dde1ed32b5a6b8b7dd757c5baec39b05c4a6
Anders Pearson <anders@thraxil.org>
2017-07-24 23:23:46 +0100

    one more simple bit

--- views_test.go
+++ views_test.go
@@ -40,6 +40,23 @@ func Test_DashboardHandler(t *testing.T) {
 	}
 }
 
+func Test_AddFormHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	n := make([]NodeData, 0)
+	_, c := makeNewClusterData(n)
+	ctx := Context{Cluster: c}
+	rec := httptest.NewRecorder()
+	AddHandler(rec, req, ctx)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
+
 func Test_FaviconHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "localhost:8080/favicon.ico", nil)
 	if err != nil {
