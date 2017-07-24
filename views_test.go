9eab6a9eeec195b669df9ca8d2185731da86e37f
Anders Pearson <anders@thraxil.org>
2017-07-24 23:04:32 +0100

    some tests for simple views

--- views_test.go
+++ views_test.go
@@ -22,3 +22,34 @@ func Test_StatusHandler(t *testing.T) {
 		t.Errorf("expected status OK; got %v", res.Status)
 	}
 }
+
+func Test_DashboardHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/dashboard/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	n := make([]NodeData, 0)
+	_, c := makeNewClusterData(n)
+	ctx := Context{Cluster: c}
+	rec := httptest.NewRecorder()
+	DashboardHandler(rec, req, ctx)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
+
+func Test_FaviconHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/favicon.ico", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	rec := httptest.NewRecorder()
+	FaviconHandler(rec, req)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
