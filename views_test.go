4ba3ad06f6150e030a42557a43d7176e0211f56b
Anders Pearson <anders@thraxil.org>
2017-07-25 15:09:05 +0100

    basic tests for announce/join GET responses

--- views_test.go
+++ views_test.go
@@ -194,3 +194,37 @@ func Test_ConfigHandler(t *testing.T) {
 		t.Errorf("expected status OK; got %v", res.Status)
 	}
 }
+
+func Test_AnnounceHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	n := make([]NodeData, 0)
+	_, c := makeNewClusterData(n)
+	ctx := Context{Cluster: c}
+	rec := httptest.NewRecorder()
+	AnnounceHandler(rec, req, ctx)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
+
+func Test_JoinHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	n := make([]NodeData, 0)
+	_, c := makeNewClusterData(n)
+	ctx := Context{Cluster: c}
+	rec := httptest.NewRecorder()
+	JoinHandler(rec, req, ctx)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
