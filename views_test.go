400242635adac38c96ab5c8e8a532e9d2b39629f
Anders Pearson <anders@thraxil.org>
2017-07-25 15:04:09 +0100

    basic coverage for ConfigHandler()

--- views_test.go
+++ views_test.go
@@ -177,3 +177,20 @@ func Test_RetrieveInfoImageHandler(t *testing.T) {
 		}
 	}
 }
+
+func Test_ConfigHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "localhost:8080/", nil)
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+	n := make([]NodeData, 0)
+	_, c := makeNewClusterData(n)
+	ctx := Context{Cluster: c}
+	rec := httptest.NewRecorder()
+	ConfigHandler(rec, req, ctx)
+
+	res := rec.Result()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
